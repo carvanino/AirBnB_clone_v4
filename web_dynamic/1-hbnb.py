4c3036b8f9b67392a2e103f779af5bfd4b89f045
jude iwuji <judeiwuji@gmail.com>
2023-01-26 14:13:55 +0100

    Update: Task 2

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -37,7 +37,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places, cache_id=cache_id)
